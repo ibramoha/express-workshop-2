d2db0fc4469292f3bf266e42d360a272d4abb974
coderyourfuture <you@example.com>
2018-01-25 01:47:23 +0000

    First commit

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const app = express();
+const posts = require('./data/posts.json');
+app.use(express.static("public", {'extensions': ['html']})); 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    posts:posts
+  });
+});
+
+
+
+app.get("/my-cv", (req, res) => {
+  res.render("my-cv");
+});
+
+
+app.get("/admin", (req, res) => {
+  res.render("admin");
+});
+
+
+// app.get("/contact", (req, res) => {
+//   res.render("contact");
+// });
+
+
 
 // The extensions 'html' allows us to serve file without adding .html at the end 
 // i.e /my-cv will server /my-cv.html
-app.use(express.static("public", {'extensions': ['html']}));
+
 
 
 // what does this line mean: process.env.PORT || 3000
